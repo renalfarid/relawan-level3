92bf82d303808ae4e096c53b29010ce8bddfd7fc
renalfarid <enal.farid@gmail.com>
2024-09-28 22:33:47 +0800

    fix koma atas

--- assets/js/formRelawan.js
+++ assets/js/formRelawan.js
@@ -442,7 +442,7 @@ document.getElementById('formNik').addEventListener('submit', async function(eve
         </tbody>
     </table>
     <div class="mt-2 flex justify-center items-center">
-     <button type="button" id="pilihPemilihBtn" onclick="populateKorTps('${pemilih[0].id}', '${pemilih[0].nama}', '${pemilih[0].alamatKtp}')" class="flex justify-items-center text-white bg-orange-600 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center">
+     <button type="button" id="pilihPemilihBtn" onclick="populateKorTps('${pemilih[0].id}', \`${pemilih[0].nama}\`, '${pemilih[0].alamatKtp.replace(/'/g, "\\'")}')" class="flex justify-items-center text-white bg-orange-600 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center">
         Pilih
         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
